90de6abd1dae41ce5f981bc95b7098b3dada322f
cc311421 <anuj.sharma01@nagarro.com>
2018-09-05 18:11:29 +0530

    first commit to add base project into repo

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// system imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// CSS imports
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
